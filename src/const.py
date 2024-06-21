eb6eb508c6df6b1985eb3bf392e017fb5296acc2
alessiocelentano <alessiocelentano2003@gmail.com>
2024-06-21 11:43:34 +0200

    Add const.py

---
+++ src/const.py
@@ -0,0 +1,8 @@
+import os
+
+SOURCE_PATH = os.path.dirname(__file__)
+PROJECT_PATH = os.path.dirname(SOURCE_PATH)
+COLLECTION_PATH = f'{PROJECT_PATH}/collection'
+
+ARTIST_DATA = f'{COLLECTION_PATH}/artist_data.csv'
+ARTWORK_DATA = f'{COLLECTION_PATH}/artwork_data.csv'
