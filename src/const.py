b758f03e9cbe241dacc10aeaaf820f4e66da0336
alessiocelentano <alessiocelentano2003@gmail.com>
2024-06-22 13:45:16 +0200

    Refactor and add functions to fill "Artworks" table

--- src/const.py
+++ src/const.py
@@ -1,8 +1,133 @@
 import os
 
+from pymysql.cursors import DictCursor
+
+
 SOURCE_PATH = os.path.dirname(__file__)
 PROJECT_PATH = os.path.dirname(SOURCE_PATH)
 COLLECTION_PATH = f'{PROJECT_PATH}/collection'
 
-ARTIST_DATA = f'{COLLECTION_PATH}/artist_data.csv'
-ARTWORK_DATA = f'{COLLECTION_PATH}/artwork_data.csv'
+HOST = '127.0.0.1'
+USER = 'root'
+PASSWORD = 'mypassword'
+DATABASE_NAME = 'museo'
+CURSOR_CLASS = DictCursor
+
+ARTISTS_DATA_CSV = f'{COLLECTION_PATH}/artist_data.csv'
+ARTWORKS_DATA_CSV = f'{COLLECTION_PATH}/artwork_data.csv'
+
+ARTISTS_TABLE_NAME = 'Artists'
+ARTWORKS_TABLE_NAME = 'Artworks'
+
+CREATE_ARTISTS_TABLE_QUERY = '''\
+    CREATE TABLE `Artists` (\
+        `id` int(6) NOT NULL,\
+        `name` varchar(255) COLLATE utf8_bin NOT NULL,\
+        `gender` varchar(6) COLLATE utf8_bin NOT NULL,\
+        `dates` varchar(255) COLLATE utf8_bin,\
+        `year_of_birth` varchar(255) COLLATE utf8_bin,\
+        `year_of_death` varchar(255) COLLATE utf8_bin,\
+        `place_of_birth` varchar(255) COLLATE utf8_bin,\
+        `place_of_death` varchar(255) COLLATE utf8_bin,\
+        `url` varchar(255) COLLATE utf8_bin NOT NULL,\
+        PRIMARY KEY (`id`)\
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin\
+'''
+
+CREATE_ARTWORKS_TABLE_QUERY = '''\
+    CREATE TABLE `Artworks` (\
+        `id` int(8) NOT NULL,\
+        `accession_number` varchar(8) COLLATE utf8_bin NOT NULL,\
+        `artist` varchar(255) COLLATE utf8_bin,\
+        `artistRole` varchar(255) COLLATE utf8_bin,\
+        `artistId` int(8) NOT NULL,\
+        `title` varchar(2047) COLLATE utf8_bin,\
+        `dateText` varchar(255) COLLATE utf8_bin,\
+        `medium` varchar(255) COLLATE utf8_bin,\
+        `creditLine` varchar(2047) COLLATE utf8_bin,\
+        `year` varchar(255) COLLATE utf8_bin,\
+        `acquisitionYear` varchar(255) COLLATE utf8_bin,\
+        `dimensions` varchar(255) COLLATE utf8_bin,\
+        `width` varchar(255) COLLATE utf8_bin,\
+        `height` varchar(255) COLLATE utf8_bin,\
+        `depth` varchar(255) COLLATE utf8_bin,\
+        `units` varchar(255) COLLATE utf8_bin,\
+        `inscription` varchar(255) COLLATE utf8_bin,\
+        `thumbnailCopyright` varchar(2047) COLLATE utf8_bin,\
+        `thumbnailUrl` varchar(255) COLLATE utf8_bin,\
+        `url` varchar(255) COLLATE utf8_bin,\
+        PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin\
+'''
+
+ARTISTS_INSERT_QUERY = '''\
+    INSERT INTO `Artists` (\
+        `id`,\
+        `name`,\
+        `gender`,\
+        `dates`,\
+        `year_of_birth`,\
+        `year_of_death`,\
+        `place_of_birth`,\
+        `place_of_death`,\
+        `url`\
+    ) VALUES (\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s\
+    )\
+'''
+
+ARTWORKS_INSERT_QUERY = '''\
+    INSERT INTO `Artworks` (\
+        `id`,\
+        `accession_number`,\
+        `artist`,\
+        `artistRole`,\
+        `artistId`,\
+        `title`,\
+        `dateText`,\
+        `medium`,\
+        `creditLine`,\
+        `year`,\
+        `acquisitionYear`,\
+        `dimensions`,\
+        `width`,\
+        `height`,\
+        `depth`,\
+        `units`,\
+        `inscription`,\
+        `thumbnailCopyright`,\
+        `thumbnailUrl`,\
+        `url`\
+    ) VALUES (\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s,\
+        %s\
+    )\
+'''
+
+SHOW_TABLE_QUERY = 'SHOW TABLES LIKE %s'
--- src/db.py
+++ src/db.py
@@ -1,44 +1,77 @@
 import csv
+
 import pymysql.cursors
 
-from const import ARTIST_DATA, ARTWORK_DATA, ARTIST_INSERT_QUERY
-
-
-HOST = '127.0.0.1'
-USER = 'root'
-PASSWORD = 'mypassword'
-DATABASE_NAME = 'museo'
-CURSOR_CLASS = pymysql.cursors.DictCursor
+from const import *
 
 
 def main():
-    connection = connect_to_mysql_db()  
-    insert_artists(connection)
+    connection = connect_to_mysql_db()
+
+    if (not table_exists(connection, ARTISTS_TABLE_NAME)):
+        create_table(connection, ARTISTS_TABLE_NAME, CREATE_ARTISTS_TABLE_QUERY)
+    insert_from_csv(connection, ARTISTS_DATA_CSV, ARTISTS_TABLE_NAME, ARTISTS_INSERT_QUERY)
+    
+    if (not table_exists(connection, ARTWORKS_TABLE_NAME)):
+        create_table(connection, ARTWORKS_TABLE_NAME, CREATE_ARTWORKS_TABLE_QUERY)
+    insert_from_csv(connection, ARTWORKS_DATA_CSV, ARTWORKS_TABLE_NAME, ARTWORKS_INSERT_QUERY)
         
         
 def connect_to_mysql_db():
-    return pymysql.connect(
-        host=HOST,
-        user=USER,
-        password=PASSWORD,
-        database=DATABASE_NAME,
-        cursorclass=CURSOR_CLASS
-    )
+    print(f'Connessione ad database "{DATABASE_NAME}"')
+
+    try:
+        connection = pymysql.connect(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            database=DATABASE_NAME,
+            cursorclass=CURSOR_CLASS
+        )
+        print(f'> Connessione all\'host {HOST} stabilita con successo')
+        return connection
+
+    except pymysql.err.OperationalError:
+        print(f'> Impossibile connettersi all\'host {HOST}. Controllare le credenziale e riprovare')
+        exit(2)
+    
+    
+def table_exists(connection, table_name):
+    print(f'\nCreazione tabella \"{table_name}"')
+    with connection.cursor() as cursor:
+        cursor.execute(SHOW_TABLE_QUERY, table_name)
+        if cursor.fetchone():
+            print(f'> Tabella "{table_name}" giá esistente')
+            return True
+        return False
+    
+    
+def create_table(connection, table_name, query):
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        connection.commit()
+    print(f'> Tabella "{table_name}" creata con successo')
 
 
-def insert_artists(connection):
-    with connection:
+def insert_from_csv(connection, csv_file, table_name, query):
+    print(f'\nInserimento delle entries nella tabella "{table_name}"')
 
-        with open(ARTIST_DATA, mode='r', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            next(reader)
+    with open(csv_file, mode='r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)
 
-            with connection.cursor() as cursor:
-                for entry in reader:
-                    cursor.execute(ARTIST_INSERT_QUERY, tuple(entry))
-                connection.commit()
-                    
-                # entries = cursor.fetchall()
+        with connection.cursor() as cursor:
+            added = duplicates = 0
+            for entry in reader:
+                try:
+                    cursor.execute(query, tuple(entry))
+                    added += 1
+                except pymysql.err.IntegrityError:
+                    duplicates += 1
+            connection.commit()
+            
+        print(f'> Aggiunte con successo {added} entries')
+        print(f'> Trovati {duplicates} duplicati')
 
 
 if __name__ == '__main__':
