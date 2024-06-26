bcc97d2b75126a9077f8e9f0a13f8c149fa5c8aa
Thomas <thomas.cantuti@gmail.com>
2024-06-26 19:53:49 +0200

    pulizia dataset

--- src/progetto.py
+++ src/progetto.py
@@ -3,6 +3,7 @@ import numpy as np
 from const import *
 from matplotlib import pyplot as plt
 import graphing
+import re
 
 
 artist_df = pd.read_csv(ARTIST_DATA)
@@ -15,6 +16,7 @@ artist_df_nan = artist_df.copy()
 artist_df_nan['gender'].fillna('Not specified', inplace=True)
 artist_df_nan['yearOfBirth'] = artist_df_nan['yearOfBirth'].replace(np.nan, 0).astype(int)
 artist_df_nan['yearOfDeath'] = artist_df_nan['yearOfDeath'].replace(np.nan, 0).astype(int)
+artist_df_nan['gender'].replace({'Male': 'M', 'Female': 'F'}, inplace=True)
 artist_df_nan['placeOfBirth'].fillna('Unknown', inplace=True)
 artist_df_nan['placeOfDeath'].fillna('Unknown', inplace=True)
 
@@ -44,10 +46,36 @@ artwork_df_nan['height'].fillna(second_number_series, inplace=True)
 artwork_df_nan['year'].fillna(0, inplace=True)
 artwork_df_nan['inscription'].fillna('date inscribed', inplace=True)
 artwork_df_nan['dimensions'].fillna('Unknown', inplace=True)
-artwork_df_nan['width'].fillna(0, inplace=True)
-artwork_df_nan['height'].fillna(0, inplace=True)
 artwork_df_nan['medium'].fillna('Unknown', inplace=True)
 artwork_df_nan['acquisitionYear'].fillna(0, inplace=True)
+artwork_df_nan['year'].replace('no date', np.nan, inplace=True)
+artwork_df_nan['year'].fillna(0, inplace=True)
+artwork_df_nan['year'] = pd.to_numeric(artwork_df_nan['year'], errors='coerce').fillna(0).astype(int)
+artwork_df_nan['acquisitionYear'] = pd.to_numeric(artwork_df_nan['acquisitionYear'], errors='coerce').fillna(0).astype(int)
+artwork_df_nan['width'] = pd.to_numeric(artwork_df_nan['width'], errors='coerce').fillna(0).astype(int)
+artwork_df_nan['height'] = pd.to_numeric(artwork_df_nan['height'], errors='coerce').fillna(0).astype(int)
+artwork_df_nan['depth'] = artwork_df_nan['depth'].astype(float).round(2)
+
+def extract_type_without_dimensions(dimension_str):
+    # Controlla se ci sono numeri nella stringa
+    if any(char.isdigit() for char in dimension_str):
+        # Estrae la parte di testo prima del primo numero
+        match = re.search(r'\d', dimension_str)
+        if match:
+            # Se trova un numero, prendi la parte di testo prima del primo numero
+            text_part = dimension_str[:match.start()].strip()
+        else:
+            text_part = dimension_str.strip()
+    else:
+        # Se non ci sono numeri, restituisci l'intera stringa
+        text_part = dimension_str.strip()
+    text_part = text_part.replace(':', '').strip()
+    return text_part if text_part else None
+
+artwork_df_nan['types'] = artwork_df_nan['dimensions'].apply(extract_type_without_dimensions)
+artwork_df_nan = artwork_df_nan[['id', 'accession_number', 'artist', 'artistRole', 'artistId', 'title', 'dateText', 'medium', 'creditLine', 'year', 'acquisitionYear', 'types', 'dimensions', 'width', 'height', 'depth', 'units', 'creditLine', 'inscription', 'url']]
+artwork_df_nan.drop(columns=['dimensions'], inplace=True)
+print(artwork_df_nan['depth'].unique())
 
 #print(artwork_df_nan.isnull().sum())
 
